e01e2828438c3b51e21ad2ec379b5c71bd6f8cbd
= <=>
2019-11-06 00:43:46 +0530

    Added universal

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,6 @@
 // import * as functions from 'firebase-functions';
 const functions = require('firebase-functions');
-const universal = require(`${process.cwd()}/dist/server.js`);
+const universal = require(`${process.cwd()}/dist/server.js`).app;
 exports.ssr = functions.https.onRequest(universal)
 
 
--- server.ts
+++ server.ts
@@ -21,7 +21,7 @@ import * as express from 'express';
 import {join} from 'path';
 
 // Express server
-const app = express();
+export const app = express();
 
 const PORT = process.env.PORT || 4000;
 const DIST_FOLDER = join(process.cwd(), 'dist/browser');
@@ -56,4 +56,3 @@ app.get('*', (req, res) => {
 app.listen(PORT, () => {
 //   console.log(`Node Express server listening on http://localhost:${PORT}`);
 });
-exports.app = app;
