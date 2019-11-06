da35c8a0939125b9250ff0a2db83b064032428f3
= <=>
2019-11-06 07:38:17 +0530

    Added Universal

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,13 +1,3 @@
-// import * as functions from 'firebase-functions';
 const functions = require('firebase-functions');
 const universal = require(`${process.cwd()}/dist/server.js`).app;
 exports.ssr = functions.https.onRequest(universal)
-
-
-
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
